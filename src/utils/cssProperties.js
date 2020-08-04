ff7869cdda8699bcaea4f291909c3f7886116e8c
Katie Cannon <katiecannon@Katies-MacBook-Air.local>
2020-08-04 20:11:37 +0100

    css properties for menu

---
+++ src/utils/cssProperties.js
@@ -0,0 +1,9 @@
+const cssPropertiesArray = ['align-content',
+ 'align-items', 
+ 'align-self', 
+ 'flex-basis', 
+ 'flex-direction', 
+ 'flex-flow',
+  'flex-grow'];
+
+ export default cssPropertiesArray;
