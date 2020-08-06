b00a9ab1ebb34ab313fd519d4029733769aa1c3c
Katie Cannon <39453428+KatieCannon@users.noreply.github.com>
2020-08-06 12:08:40 +0100

    alignSelf subheader values

--- src/Components/AlignSelf/AlignSelf.js
+++ src/Components/AlignSelf/AlignSelf.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './AlignSelf.css';
+import SubHeader from '../SubHeader/SubHeader';
 
+const alignSelfValues = ['center', 'stretch', 'flex-start', 'flex-end', 'baseline'];
 const AlignSelf = () => {
     return(
-        <div>
-            <h1>inside AlignSelf</h1>
+        <div className='alignSelf'>
+            <p className='alignSelf__about'>A sub property of the Flexible Box Layout Module that aligns a single element within a flex container.</p>
+            {alignSelfValues.map(value => (
+                <div key={value} >
+                    <SubHeader headerValue={value} />
+                </div>
+            ))}
         </div>
     )
 }
