56716037dce6b29634d66dfe4296932e99cfa925
Katie Cannon <39453428+KatieCannon@users.noreply.github.com>
2020-08-06 10:45:44 +0100

    putting before and after images next to each other

--- src/Components/AlignContent/AlignContent.js
+++ src/Components/AlignContent/AlignContent.js
@@ -16,14 +16,22 @@ const AlignContent = () => {
     return (
         <div className='alignContent'>
             <p className='alignContent__about'>A sub property of the Flexible Box Layout Module. It only applies where there are multiple lines. This property has no effect on single line flex containers (i.e. ones with flex-wrap: nowrap).</p>
-            <p className='alignContent__about'>The below image shows 6 items with display: flex and flex: wrap</p>
-            <div className='alignContent__example'><AlignContentExample value='none' /></div>
+            <p className='alignContent__about'>The below images shows 6 items with display: flex and flex: wrap</p>
            {alignContentValues.map(value => (
                <div key={value}>
                    <SubHeader headerValue={value} />
                    <p className='alignContent__description'>{exampleDescription[value]}</p>
-                   <div className='alignContent__example'>
-                        <AlignContentExample value={value}/>
+                   <div className='alignContents__exampleContainerLabels'>
+                       <p>Before</p>
+                       <p>After</p>
+                    </div>
+                    <div className='alignContent__exampleContainer'>
+                        <div className='alignContent__example'>
+                            <AlignContentExample value='none'/>
+                        </div>
+                        <div className='alignContent__example'>
+                           <AlignContentExample value={value}/>
+                        </div>
                     </div>
                 </div>
            ))}
