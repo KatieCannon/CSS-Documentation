b3238eca62ae3233a9c8734e2dd5f3c485d74869
Katie Cannon <39453428+KatieCannon@users.noreply.github.com>
2020-08-06 11:53:44 +0100

    subheaders for alignItems

--- src/Components/AlignItems/AlignItems.js
+++ src/Components/AlignItems/AlignItems.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './AlignItems.css';
+import SubHeader from '../SubHeader/SubHeader';
 
+const alignItemsValues = ['center', 'flex-start', 'flex-end', 'baseline', 'stretch'];
 const AlignItems = () => {
     return (
-        <div>
-            <h1>align-items</h1>
+        <div className='alignItems'>
+            <p className='alignItems__about'>A sub property of the Flexible Box Layout Module that controls how items are positioned along a flex container's cross axis.</p>
+            {alignItemsValues.map(value => (
+                <div key={value}>
+                    <SubHeader headerValue={value} />
+                </div>
+            ))}
         </div>
     )
 }
